2d83777d09837d5f27ef9eb3646a1ea90dd4891f
yks095 <rltjr219@naver.com>
2019-10-10 14:49:41 +0900

    1010/ch4_solving..(15/19)

---
+++ src/ch4/Q14.java
@@ -0,0 +1,24 @@
+package ch4;
+
+public class Q14 {
+    public static void main(String[] args) {
+        SimpleStack stack = new SimpleStack(3); // 크기 10의 Stack 생성
+
+        // 스택에 5 9 1 순차 삽입
+        stack.push(5);
+        stack.push(9);
+        stack.push(1);
+        System.out.println(stack); // 스택 출력
+
+        int peekData = stack.peek();
+        int data=stack.pop(); // 스택에서 자료 추출
+
+        System.out.println("Data peeked from stack : " + peekData);
+        System.out.println("Data deleted from stack : " + data);
+        System.out.println(stack); // 스택 출력
+
+        System.out.println("stack empty test : " + stack.isEmpty());
+        System.out.println("stack full test : " + stack.isFull());
+
+    }
+}
---
+++ src/ch4/SimpleStack.java
@@ -0,0 +1,43 @@
+package ch4;
+
+import java.util.Arrays;
+
+public class SimpleStack {
+    int stack[];
+    int top =- 1;
+
+    public SimpleStack(int size) {
+        stack = new int[size];
+    }
+
+    public void push(int data) {
+        stack[++top] = data;
+    }
+
+    public int pop() {
+        return stack[top--];
+    }
+
+    public boolean isEmpty()   {
+        if(top == -1)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isFull()    {
+        if(top == stack.length - 1)
+            return true;
+        else
+            return false;
+    }
+
+    public int peek()  {
+        return stack[top];
+    }
+
+    @Override
+    public String toString() {
+        return "top = " + top + ", stack = "+ Arrays.toString(stack);
+    }
+}
